6311b9933d23291c5284b0cf82c970366295e1cc
Unknown15082 <trangiahuy15082006@gmail.com>
2023-06-10 23:49:17 +0700

    Ctrl-S keybinds

--- lua/core/keybinds.lua
+++ lua/core/keybinds.lua
@@ -1,4 +1,11 @@
--- Set <leader> to <space>
+-- Unmap <Space>
+vim.keymap.set("n", " ", "<Nop>", { silent = true, remap = false })
+
+-- Set <leader> to <Space>
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+-- Save with <C-s>
+vim.keymap.set("n", "<C-s>", "<Cmd>w<CR>")
+vim.keymap.set("i", "<C-s>", "<Esc><Cmd>w<CR>a")
 
