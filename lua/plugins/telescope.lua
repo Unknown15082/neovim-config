8df016778c0f1bad316311f4834027088d744d6b
Unknown15082 <trangiahuy15082006@gmail.com>
2023-06-12 21:35:07 +0700

    Telescope file browser

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,7 @@ return {
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-telescope/telescope-fzf-native.nvim",
+			"nvim-telescope/telescope-file-browser.nvim",
 
 			"nvim-tree/nvim-web-devicons",
 		},
@@ -25,6 +26,13 @@ return {
 					require("telescope.builtin").find_files({ cwd = '~', hidden = false })
 				end,
 				desc = "Find files (home)",
+			},
+			{
+				"<leader>ft",
+				function()
+					require("telescope").extensions.file_browser.file_browser()
+				end,
+				desc = "Find browser tree",
 			},
 			-- UI
 			{
@@ -102,12 +110,63 @@ return {
 					override_file_sorter = true,
 					case_mode = "smart_case",
 				},
+				file_browser = {
+					git_status = true,
+					use_fd = true,
+					hijack_netrw = true,
+
+					hidden = {
+						file_browser = true,
+						folder_browser = true,
+					},
+					display_stat = {
+						size = true,
+						date = false,
+						mode = false,
+					},
+					mappings = {},
+				}
 			},
 		},
+		config = function(_, opts)
+			opts = vim.tbl_deep_extend(
+				"force",
+				vim.deepcopy(opts).extensions.file_browser.mappings,
+				{
+					["n"] = {
+						["a"] = require "telescope".extensions.file_browser.actions.create, -- [A]dd
+						["r"] = require "telescope".extensions.file_browser.actions.rename, -- [R]ename
+						["m"] = require "telescope".extensions.file_browser.actions.move,   -- [M]ove
+						["y"] = require "telescope".extensions.file_browser.actions.copy,   -- [Y]ank
+						["d"] = require "telescope".extensions.file_browser.actions.remove, -- [D]elete
+						["o"] = require "telescope".extensions.file_browser.actions.open,   -- [O]pen
+						["<BS>"] = require "telescope".extensions.file_browser.actions.goto_parent_dir,
+						["e"] = require "telescope".extensions.file_browser.actions.goto_home_dir, -- default
+						["c"] = require "telescope".extensions.file_browser.actions.goto_cwd, -- [C]wd
+						["."] = require "telescope".extensions.file_browser.actions.change_cwd,
+						["f"] = require "telescope".extensions.file_browser.actions.toggle_browser, -- [F]ile/[F]older
+						["h"] = require "telescope".extensions.file_browser.actions.toggle_hidden, -- [H]idden
+						["s"] = require "telescope".extensions.file_browser.actions.toggle_all, -- default
+					},
+				}
+			)
+			require("telescope").setup(opts)
+
+			require("telescope").load_extension("fzf")
+			require("telescope").load_extension("file_browser")
+		end,
 	},
 	{
 		"nvim-telescope/telescope-fzf-native.nvim",
 		lazy = true,
 		build = "make",
+	},
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		lazy = true,
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			"nvim-lua/plenary.nvim",
+		},
 	},
 }
