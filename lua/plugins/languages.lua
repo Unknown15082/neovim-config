64b4f5f3f703a0b45d80c3886e6b27038558a999
Unknown15082 <trangiahuy15082006@gmail.com>
2023-08-02 18:18:24 +0700

    Languages settings

---
+++ lua/plugins/languages.lua
@@ -0,0 +1,14 @@
+-- Configuration for some programming languages
+
+return {
+	-- C/C++
+	{ import = "lazyvim.plugins.extras.lang.clangd" },
+	-- Go
+	{ import = "lazyvim.plugins.extras.lang.go" },
+	-- Python
+	{ import = "lazyvim.plugins.extras.lang.python" },
+	-- Rust
+	{ import = "lazyvim.plugins.extras.lang.rust" },
+	-- LaTeX
+	{ import = "lazyvim.plugins.extras.lang.tex" },
+}
