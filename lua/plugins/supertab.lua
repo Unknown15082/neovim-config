a41169333a18004b3115e54f1d7e0bfd53d8ebd2
Unknown15082 <trangiahuy15082006@gmail.com>
2023-08-02 18:06:45 +0700

    Remove cmp-emoji

--- lua/plugins/supertab.lua
+++ lua/plugins/supertab.lua
@@ -12,9 +12,6 @@ return {
 	-- Set up supertab
 	{
 		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-emoji",
-		},
 		---@param opts cmp.ConfigSchema
 		opts = function(_, opts)
 			local has_words_before = function()
